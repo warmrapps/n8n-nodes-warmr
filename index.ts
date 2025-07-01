7cd089cd813d0be0b73fba488249808055d71a2b
Bérenger Stasse <hello@berenger.dev>
2025-07-01 12:28:18 +0200

    Add WarmrApiCredentials to nodeTypes and update package version to 2.0.16, refining build output configuration in package.json.

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 import { ContactsNode } from "./nodes/Contacts.node";
+import { WarmrApiCredentials } from "./credentials/WarmrApi.credentials";
 
 export const nodeTypes = [ContactsNode];
+export const credentialTypes = [WarmrApiCredentials];
